1c3e3408c6adf849055d451bc2eac7f50f7371be
ChristinaMifsud <christina.mifsud@gmail.com>
2023-04-06 18:58:55 +0100

    Exercises completed

--- 1-exercises/exercise.js
+++ 1-exercises/exercise.js
@@ -23,6 +23,26 @@ Write JavaScript below that console.logs:
 
 */
 
+const elements = document.querySelectorAll("p");
+for (const element of elements) {
+  console.log(element);
+}
+
+const firstElement = document.querySelector("div");
+console.log(firstElement);
+
+// const elementsInner = document.querySelectorAll("p");
+// for (const element of elements) {
+//   console.log(element.innerText);
+// }
+
+const jumbotronText = document.querySelector("#jumbotronText");
+console.log(jumbotronText);
+
+const itemsClass = document.querySelectorAll(".primary-content");
+for (const item of itemsClass) {
+  console.log(item);
+}
 
 /*
 Task 2
@@ -31,6 +51,11 @@ Task 2
 When a user clicks the 'ALERT' button, an alert box should pop up with the text "Thanks for visiting Bikes for Refugees!"
 */
 
+let alertBtn = document.querySelector("#alertBtn");
+
+alertBtn.addEventListener("click", function () {
+  alert("Thanks for visiting Bikes for Refugees!");
+});
 
 /*
 Task 3
@@ -40,6 +65,11 @@ When a user clicks the 'Change colour' button, the background colour of the page
 Hint: can you do this with a CSS class instead of adding styles to the element?
 */
 
+let makeRed = document.querySelector("#bgrChangeBtn");
+
+makeRed.addEventListener("click", function () {
+  document.body.classList.add("background--red");
+});
 
 /*
 Task 4
@@ -48,6 +78,14 @@ Task 4
 When a user clicks the 'Larger links!' button, the text of all links on the page should increase.
 */
 
+let largerLinksBtn = document.querySelector("#largerLinksBtn");
+
+largerLinksBtn.addEventListener("click", function () {
+  for (const link of document.querySelectorAll("a")) {
+    link.classList.add("larger");
+    //console.log(link.classList);
+  }
+});
 
 /*
 Task 5
@@ -56,3 +94,16 @@ Task 5
 When a user clicks the 'Add some text' button, the text in the input field should be appended to the page as a new paragraph below it.
 Hint: the new paragraph should be appended to the element with id 'addArticle'.
 */
+
+addArticleBtn.addEventListener("click", function (event) {
+  event.preventDefault(); // prevents page from refreshing when we click the button
+
+  const inputElement = document.querySelector("#addArticleInput");
+
+  // create a new paragraph element
+  const pElement = document.createElement("p");
+  // set the innerText to be the input's value
+  pElement.innerText = inputElement.value;
+  // add it to the page
+  document.getElementById("addArticle").appendChild(pElement);
+});
